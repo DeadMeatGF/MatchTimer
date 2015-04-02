9b9c95828c24b1c1c894598c10642238c98b5769
DeadMeatGF <deadmeatgf.coding@gmail.com>
2015-04-02 13:04:46 +0100

    Editable ListView foundation in place.

--- app/src/main/java/uk/co/stevegiller/deadmeatgf/matchtimer/MatchActivity.java
+++ app/src/main/java/uk/co/stevegiller/deadmeatgf/matchtimer/MatchActivity.java
@@ -1,5 +1,6 @@
 package uk.co.stevegiller.deadmeatgf.matchtimer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,6 +28,18 @@ public class MatchActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_app_settings:
+                return true;
+            case R.id.action_match_settings:
+                return true;
+            case R.id.action_settings_home_team:
+                return true;
+            case R.id.action_settings_away_team:
+                Intent intent = new Intent(this, TeamListActivity.class);
+                startActivity(intent);
+                return true;
+        }
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
---
+++ app/src/main/java/uk/co/stevegiller/deadmeatgf/matchtimer/TeamListActivity.java
@@ -0,0 +1,102 @@
+package uk.co.stevegiller.deadmeatgf.matchtimer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class TeamListActivity extends Activity {
+    private ListView myList;
+    private MyAdapter myAdapter;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_list);
+
+        myList = (ListView) findViewById(R.id.listView);
+        myList.setItemsCanFocus(true);
+        myAdapter = new MyAdapter();
+        myList.setAdapter(myAdapter);
+
+    }
+
+    public class MyAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+        public ArrayList myItems = new ArrayList();
+
+        public MyAdapter() {
+            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            for (int i = 0; i < 20; i++) {
+                ListItem listItem = new ListItem();
+                listItem.caption = "Caption" + i;
+                myItems.add(listItem);
+            }
+            notifyDataSetChanged();
+        }
+
+        public int getCount() {
+            return myItems.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = mInflater.inflate(R.layout.list_player, null);
+                holder.caption = (EditText) convertView
+                        .findViewById(R.id.player_name);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            //Fill EditText with the value you have in data source
+//            holder.caption.setText(myItems.get(position).caption);
+            holder.caption.setText("Player Name");
+            holder.caption.setId(position);
+
+            //we need to update adapter once we finish with editing
+            holder.caption.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus){
+                        final int position = v.getId();
+                        final EditText Caption = (EditText) v;
+//                        myItems.get(position).caption = Caption.getText().toString();
+                        Toast.makeText(getApplicationContext(), Caption.getText().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            return convertView;
+        }
+    }
+
+    class ViewHolder {
+        EditText caption;
+    }
+
+    class ListItem {
+        String caption;
+    }
+}
